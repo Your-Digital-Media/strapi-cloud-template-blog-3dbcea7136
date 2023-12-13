c9f3aaf8a74392cbfccfa861b4796eab738f1c72
yourdigitalmedia <alex@yourdigitalmedia.com.au>
2023-12-13 20:01:57 +0100

    Initial attempt for PoC

---
+++ src/api/body-colour/routes/body-colour.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * body-colour router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::body-colour.body-colour');
---
+++ src/api/body-colour/services/body-colour.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * body-colour service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::body-colour.body-colour');
